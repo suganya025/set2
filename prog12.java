edbe7de7a27d32c4e40878e9ce8f163c490cb6cd
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-25 20:48:42 +0530

    Create prog12.java

---
+++ prog12.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class palindrome
+{
+	public static void main(String[] args) 
+	{   
+		Scanner ob = new Scanner(System.in);
+		int temp,sum=0,a;
+   	int n = ob.nextInt();
+  	temp=n;
+  		if(n<=1000)
+  		{
+          while(n>0)
+          {    
+            a=n%10;  
+            sum=(sum*10)+a;    
+            n=n/10;    
+           }    
+  		}
+     if(temp==sum)    
+        System.out.println("yes");    
+     else    
+        System.out.println("no");    
+	}
+}  
