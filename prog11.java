baabbff624ecc7ebcc9f92c0ae92cadad15b1100
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-25 20:13:32 +0530

    Create prog11.java

---
+++ prog11.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class powern
+{
+	public static void main(String[] args) 
+	{   
+		Scanner ob = new Scanner(System.in);
+   		int base = ob.nextInt();
+  		int k = ob.nextInt();
+  		double result = Math.pow(base, k);
+    	System.out.println(+result);
+    }
+}
