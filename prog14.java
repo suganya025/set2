14ab8f87b35fbbfdb10e7848ca56687b0666638d
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-25 21:32:45 +0530

    Update prog14.java

--- prog14.java
+++ prog14.java
@@ -6,14 +6,18 @@ class oddnum
 	public static void main(String[] args) 
 	{   
 		Scanner ob = new Scanner(System.in);
-        int a=ob.nextInt();
-        int b=ob.nextInt();
-        for (int i=a; i < b; i++)
-        {
-            if( i % 2 != 0)
-            {
-				        System.out.print(i+" ");
-            }
-        }
+        	int a=ob.nextInt();
+        	int b=ob.nextInt();
+       		for (int i=a; i<b;)
+        	{
+        		++i;
+        		if(i!=b)
+        		{
+           		 if( i % 2 != 0)
+            		{
+				System.out.print(i+" ");
+           	        }
+        		}
+        	}
 	}
 }  
